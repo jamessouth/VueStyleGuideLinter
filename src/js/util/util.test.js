d3e3a254ae3a636caa97daa6af1a4538d6c07072
jamessouth <jamesdanielsouth@gmail.com>
2019-02-09 15:54:47 -0600

    begin test

--- src/js/util/util.test.js
+++ src/js/util/util.test.js
@@ -1,7 +1,20 @@
 import test from 'ava';
+import errorHandler from './errorHandler';
+import getComponentName from './getComponentName';
+import getComponentProperties from './getComponentProperties';
+import getComponentPropNames from './getComponentPropNames';
+import getScript from './getScript';
+import isFilePath from './isFilePath';
+import pipe from './pipe';
 import removeBuggyKeywords from './removeBuggyKeywords';
+import resultReporter from './resultReporter';
+
+
+
+
+
 
 test('test removeBuggyKeywords', t => {
-  const str = 'hello await async import world';
+  const str = 'helloawaitasyncimport world';
   t.is(removeBuggyKeywords(str), 'hello world');
 });
