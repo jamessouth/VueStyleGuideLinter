531da7f51d03a59d3cdc6848cd06f4234c2b066f
jamessouth <jamesdanielsouth@gmail.com>
2019-02-14 02:01:27 -0600

    revise bin

--- bin/vue-style-guide-linter.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-const VSL = require('../src/js/index').default;
-
-VSL(process.argv[2]);
---
+++ index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const VSL = require('./src/js/vsl').default;
+
+VSL(process.argv[2]);
