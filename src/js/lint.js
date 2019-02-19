635ec7c5e84e0460a71d3e593daf107a80eaefd1
jamessouth <jamesdanielsouth@gmail.com>
2019-02-19 13:42:52 -0600

    finish refactor

--- src/js/lint.js
+++ src/js/lint.js
@@ -1,13 +1,10 @@
-import errorHandler from './util/errorHandler';
 import isFilePath from './util/isFilePath';
 import lintFile from './lintFile';
 import lintDirectory from './lintDirectory';
 
-const fsProm = require('fs').promises;
-
 export default async function lint(arg) {
   if (!await isFilePath(arg)) {
-    return lintDirectory(arg);
+    return lintDirectory(arg, lintFile);
   }
   return lintFile(arg);
 }
--- src/js/lintDirectory.js
+++ src/js/lintDirectory.js
@@ -1,14 +1,14 @@
 import pipe from './util/pipe';
-import lintFile from './lintFile';
+import errorHandler from './util/errorHandler';
 
 const fsProm = require('fs').promises;
 
-export default async function lintDirectory(arg) {
+export default async function lintDirectory(arg, lintFunc) {
   try {
     const myDirName = /\/$/.test(arg) ? arg : `${arg}/`;
     const buildFullPath = file => myDirName + file;
     const myDir = await fsProm.readdir(arg);
-    return Promise.all(myDir.map(pipe(buildFullPath, lintFile)));
+    return Promise.all(myDir.map(pipe(buildFullPath, lintFunc)));
   } catch (err) {
     return errorHandler(err.message, 'directory');
   }
